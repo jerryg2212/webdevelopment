1af6499a2afe404f4d5aa564fe37abd10a8b7ed1
Jerry Gensiejewski <jerryg2212@gmail.com>
2021-05-01 14:49:16 -0400

    lkj

--- practiceweb/My own websites/SpotifyAPI/index.js
+++ practiceweb/My own websites/SpotifyAPI/index.js
@@ -5,6 +5,6 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile('homepage.html', {root : 'public'});
-})
+});
 
 app.listen(8000, () => {console.log('listening on port 8000')})
