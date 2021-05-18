c6e15b33979d736fd398385e4e30c1a42f739796
Jerry Gensiejewski <jerryg2212@gmail.com>
2021-05-17 23:07:49 -0400

    s

--- practiceweb/My own websites/SpotifyAPI/herokuindex.js
+++ practiceweb/My own websites/SpotifyAPI/herokuindex.js
@@ -6,9 +6,12 @@ const queryString = require('querystring');
 
 const port = process.env.PORT || 5000;
 
-//const clientId = process.env.CLIENT_ID;
+//const clientId = j  process.env.CLIENT_ID;
 //const clientSecret = process.env.CLIENT_SECRET;
 //const state = process.env.SECRET_CODE;
+const clientId = 'j';
+const clientSecret = 'j';
+const state = 'j';
 const redirecturi = 'https://jerrysspotifyapi.herokuapp.com/callback';
 const getTokenURL = 'https://accounts.spotify.com/api/token'
 const scopes = {
