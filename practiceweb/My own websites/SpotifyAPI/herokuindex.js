ece7b630344cdbb42e3f15629d4011c4ca624b0d
Jerry Gensiejewski <jerryg2212@gmail.com>
2021-05-17 22:50:30 -0400

    s

--- practiceweb/My own websites/SpotifyAPI/herokuindex.js
+++ practiceweb/My own websites/SpotifyAPI/herokuindex.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const request = require('request');
 const queryString = require('querystring');
-require('dotenv').config();
+//require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
