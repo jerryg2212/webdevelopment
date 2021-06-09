0a3768d15d46945abe7e43fc036bc8d49b52e70b
Jerry Gensiejewski <jerryg2212@gmail.com>
2021-06-09 19:45:19 -0400

    big league chew not done just stopped

--- practiceweb/Practice-React/bigleaguechewreact/src/index.js
+++ practiceweb/Practice-React/bigleaguechewreact/src/index.js
@@ -1,17 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import logo from './logo.png';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//container that wraps the navbar
+class NavBarContainer extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return <div id={this.props.navBarContainerId}><NavBarLogo logoId="logo"/><NavBarList childOne={<NavBarListElement text="home"/>}/></div>
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//logo element
+class NavBarLogo extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return <img src={logo} id={this.props.logoId}></img>
+  }
+}
+
+// list of elements on the nav bar
+class NavBarList extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {hover : false}
+  }
+  onMouseOverEvent = () => {
+    this.setState({hover : true});
+    this.render();
+  }
+  onMouseOutEvent = () => {
+    this.state.hover = false;
+    console.log(`on mouse out ran and the hover is ${this.state.hover}`);
+    this.render();
+
+  }
+  render(){
+    console.log(`render ran and the hover is ${this.state.hover}`);
+    return  <ul onMouseOver={this.onMouseOverEvent} onMouseOut={this.onMouseOutEvent}>
+              <NavBarListElement text="Home" hover={this.state.hover}/><NavBarListElement text="About" hover={this.state.hover}/>
+              <NavBarListElement text="Products" hover={this.state.hover}/><NavBarListElement text="Buy/Sell" hover={this.state.hover}/>
+              <NavBarListElement text="News" hover={this.state.hover}/><NavBarListElement text="Contact" hover={this.state.hover}/>
+            </ul>
+  }
+}
+
+// list element in the nav bar list
+class NavBarListElement extends React.Component{
+  constructor(props){
+    super(props);
+    console.log(`this is list elements constructor and hover is ${this.props.hover}`);
+    if(this.props.hover){
+      this.state = {class : 'navBarListElementColor'}
+    }else{
+      this.state = {class : ''}
+    }
+  }
+  /*static getDerivedStateFromProps(props, state){
+    if(props.hover){
+      return {class : "navBarListElementColor"}
+    }
+    else return
+  }*/
+  render(){
+    return <li className={this.state.class}>{this.props.text}</li>
+  }
+}
+
+
+ReactDOM.render(<NavBarContainer navBarContainerId="navBarContainer"/>, document.getElementById('root'));
