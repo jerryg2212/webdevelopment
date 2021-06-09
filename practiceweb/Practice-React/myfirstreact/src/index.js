226b0f20a32948c41983df26d9671618ebf60f9a
Jerry Gensiejewski <jerryg2212@gmail.com>
2021-06-09 10:55:37 -0400

    housekeeping

--- practiceweb/Practice-React/myfirstreact/src/index.js
+++ practiceweb/Practice-React/myfirstreact/src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 
 const headerElm = <h1>fuck you</h1>;
 
-ReactDOM.render(
-  headerElm,
-  document.getElementById('root')
-);
+
+
+class TestComponent extends React.Component {
+  constructor(){
+    super();
+    this.state = {color: "red"}
+  }
+  render(){
+    return <h2>this is an h2 element {this.props.shit}</h2>
+  }
+}
+
+
+ReactDOM.render(<TestComponent shit="onMe"/>,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
